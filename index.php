f6fc1eaff37ad410a1a914d3616298689aaf3e2d
youngil kim <94youngil@gmail.com>
2024-03-23 00:35:01 +0900

    06 test

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
         <title>title</title>
     </head>
     <body>
-<<<<<<< HEAD
+
         <h2>test<h2>
 
         haha
         hoho
         hihi
         fufu
-=======
+
         <h2>test</h2>
         
         <h2>신입사원의 테스트</h2>
->>>>>>> ff9292cc0d65f88dd830b64e5b2141d121c50915
+        
+        ㅋㅋ
+
     </body>
 </html>
